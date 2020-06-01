2bac27dc54facc0bab9c46e83e75db3605390149
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-01 17:48:28 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,5 +1,6 @@
 class Teacher < User #class inherits from the User class.
 #inherits the user method which allows the Teacher to have a first and last name.
+
 KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 #class constant kNOWLEDGE.
 
