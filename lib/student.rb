a23ce61cd5a9d6e9711e382ad6994bae25ad1e72
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-01 17:47:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student  < User #changes the class definition so that it inherits from the
     @knowledge = [] #initializes with instance variable knowledge, points to an empty array.
   end
 
-  def learn(string) #takes in a string and adds to that string to the student's knowledge array. 
-    knowledge << string
+  def learn(string) #takes in a string and adds to that string to the student's knowledge array.
+    knowledge << string 
   end
 
 end
