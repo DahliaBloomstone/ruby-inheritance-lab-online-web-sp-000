e6306c03241ddad3886465dd475df8143b168e98
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-01 17:46:02 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,9 +1,10 @@
 class Teacher < User #class inherits from the User class.
 
 KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+#class constant kNOWLEDGE.
 
-def teach
-  KNOWLEDGE.sample
+def teach #returns a random element from that array.
+  KNOWLEDGE.sample #.sample method returns a random element from an array. 
 end
 
 end
