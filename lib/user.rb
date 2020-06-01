4d3c590f31b33d47066e858df9174143844c456c
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-01 17:42:57 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,8 @@
 class User #super class! Parent
 attr_accessor :first_name, :last_name
 
-def initialize(first_name, last_name)
+def initialize(first_name = nil, last_name = nil)
 @first_name = first_name
 @last_name = last_name
 end
+end 
