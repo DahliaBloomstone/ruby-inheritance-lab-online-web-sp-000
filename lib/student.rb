991751285fc2747551a5b0a80afc0b72e1b27cdc
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-01 17:46:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
-class Student  < User #changes the class definition so that it inherits from the user. 
+class Student  < User #changes the class definition so that it inherits from the user.
   attr_accessor :knowledge
 
   def initialize
-    @knowledge = []
+    @knowledge = [] #initializes with instance variable knowledge, points to an empty array.
   end
 
-  def learn(string)
+  def learn(string) #takes in a string and adds to that string to the student's knowledge array. 
     knowledge << string
   end
 
