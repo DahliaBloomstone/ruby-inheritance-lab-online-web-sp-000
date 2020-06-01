530be95f1a813b0e84de17ec52db83dac309943a
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-01 17:48:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,5 @@
 class Student  < User #changes the class definition so that it inherits from the user.
+#inherits: the methods so that the student will have a first and last name. 
   attr_accessor :knowledge
 
   def initialize
